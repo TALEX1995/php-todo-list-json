379e2d8cc731bed973e62908367e6967eaab730d
TALEX1995 <tanase.alex@libero.it>
2023-07-20 16:38:05 +0200

    Save new task in database tasks.json

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -16,6 +16,10 @@ $new_task = $_POST['task'] ?? null;
 // Add new task
 if ($new_task) {
     $tasks[] = $new_task;
+
+    // Add tasks array with new task in tasks.json
+    $json_tasks = json_encode($tasks);
+    file_put_contents($database_path, $json_tasks);
 }
 
 // Specific type file
